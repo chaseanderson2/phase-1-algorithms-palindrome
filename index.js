b1482074d896b9500fc99abc63a327aa26066255
Chase <chaseanderson.stu@gmail.com>
2024-04-03 18:46:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+const { consoleLog } = require("mocha/lib/reporters/base");
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase();
+
+  let left = 0;
+  let right = word.length - 1;
+
+  while (left < right) {
+    if (word[left] !== word[right]){
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  create a function that takes 1 string as an argument
+    make sure the string consists of lowercase letters only
+
+    create two variables that compares the characters at the start of the
+    string to the characters at the end of the string
+
+    compare the characters
+      if they don't match(not a palindrome), return false
+
+      if they do match(are a palindrome), return true
+
 */
 
 /*
-  Add written explanation of your solution here
+  The function isPalindrome takes one string that is meant to be a word. 
+  It immediately converts all of the characters in the string to be lowercase
+  for simplicity's sake, it makes it easier for comparing the characters against
+  each other. Then i created two variables, left and right, to help look at the characters
+  in front of the function as well as the end. Then i write a while statement that comapres
+  the characters from the outside of the word, inward until it reaches the middle. Inside
+  of the while statement there is an if statement. It compares the characters, and if it does
+  not return a palindrome, it will return false. If it does return a palindrome, it will return 
+  true.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +48,25 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("hannah"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("chase"));
+
+
 }
 
 module.exports = isPalindrome;
